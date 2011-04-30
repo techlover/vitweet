a231917e7a1a479dba20fb2b8f8155fb4e9be5f3
Gabriel Chavez <gabrielchavez02@gmail.com>
2011-04-30 01:55:03 -0500

    Forgot to add the line of the tweet

--- test.c
+++ test.c
@@ -5,6 +5,7 @@ const char[] consumer_key = "KAQaEW6OyzNn5QIibXKbg";
 const char[] consumer_secret = "t5ddxhIeFSJDnVweVBRAuBuytsFK1a1vUoflx78";
 const char[] request_token = "https://api.twitter.com/oauth/request_token";
 const char[] access_token = "https://api.twitter.com/oauth/access_token";
+const char[] tweet = "My first tweet from my own app";
 
 int main(int argc, char *argv[])
 {
