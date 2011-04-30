a3b3f71bd275d6b1bc24932de79af1c916a45365
Gabriel Chavez <gabrielchavez02@gmail.com>
2011-04-30 01:44:56 -0500

    First files created, I'm going to play with the api first

---
+++ test.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+
+const char[] consumer_key = "KAQaEW6OyzNn5QIibXKbg";
+const char[] consumer_secret = "t5ddxhIeFSJDnVweVBRAuBuytsFK1a1vUoflx78";
+const char[] request_token = "https://api.twitter.com/oauth/request_token";
+const char[] access_token = "https://api.twitter.com/oauth/access_token";
+
+int main(int argc, char *argv[])
+{
+    
+    return 0;
+}
